38a723b4abefa91b934b0ad6e7baf52786d4acca
Duncan <59705125+DuncanRuns@users.noreply.github.com>
2025-03-31 11:02:52 -0300

    Update Atum.java

--- src/main/java/me/voidxwalker/autoreset/Atum.java
+++ src/main/java/me/voidxwalker/autoreset/Atum.java
@@ -7,16 +7,11 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.options.KeyBinding;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.lwjgl.glfw.GLFW;
 
 import java.util.Objects;
-import java.util.Optional;
-import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 
 public class Atum implements ClientModInitializer {
